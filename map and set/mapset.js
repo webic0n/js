154261524e6e4301b842d595ffab426f8e80fb28
John Doe <vaza19max@gmail.com>
2020-01-30 00:11:52 +0300

    mapAndSet

---
+++ map and set/mapset.js
@@ -0,0 +1,37 @@
+///zad 1
+// let unique = ( arr ) => {
+//     return Array.from(new Set(arr));
+   
+// }
+// let values = ["Hare", "Krishna", "Hare", "Krishna",
+//   "Krishna", "Krishna", "Hare", "Hare", ":-O"
+// ];
+
+// alert( unique(values) );
+///zad 2
+// let arr = [
+//     ["fef","frf"],
+//     [1,"goga"],
+//     ["gg","gf"],
+//     [{name : "max"},"fhf"],
+    
+// ]
+// let map = new Map(arr)
+// let mass = [
+//     {name : "goga"  },
+//     {name : "fina"  },  
+//     {name : "joj"  }     
+// ]
+// let maps = new Map()
+// maps.set(mass[2],new Date(new Date().getTime() + 22222222221*2/2 ));
+// const f = (a) =>  maps.get(a);
+// console.log(f(mass[2]));
+// let mass = Array.from(map);
+// let goga = Object.fromEntries(map.entries());
+// console.log(goga)
+// map.forEach((zn,key,arr) => {
+//     console.log(zn,key);
+// });
+// let set = new Set([1,2,3,4,5,2,3,1,4,5]);
+// set.add(10).add(10).add(20).add(20);
+// console.log(set.entries());
